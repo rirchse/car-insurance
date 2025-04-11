b36721602246a2f7fcebe3b20b8647f4e75f740f
rirchse <rirchse@gmail.com>
2025-04-11 17:59:07 +0600

    address auto load

--- index.php
+++ index.php
@@ -272,7 +272,7 @@
         const zipcodefile = 'zipcode.json?v=1.10';
         const jsonfile = 'merged_make_year_model.json';
       </script>
-      <script src="calculation-scripts.js?v=0.130"></script>
+      <script src="calculation-scripts.js?v=0.140"></script>
       
       <?php } else { ?>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles.css">
