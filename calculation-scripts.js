3b626c3c9a9617063357d146dfb7e4aaaee94af0
rirchse <rirchse@gmail.com>
2025-04-11 18:22:11 +0600

    Load issue solve for brand

--- calculation-scripts.js
+++ calculation-scripts.js
@@ -187,7 +187,7 @@ function brands(e)
     let moreBtn = document.createElement('div');
     moreBtn.setAttribute('class', 'more-options inner-wrap-btn');
     moreBtn.innerHTML = 
-      '<button class="show-more" name="more" onclick="brands(this)">'+
+      '<button class="show-more" value="more" onclick="brands(this)">'+
         '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 +24 24" stroke-width="1.5" stroke="currentColor" class="size-6">'+
           '<path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />'+
           '</svg>'+
